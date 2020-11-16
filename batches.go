7fe0513173e7b87f9abf11d75b28feb7f8c24953
Till Klocke <till.klocke@gmail.com>
2020-11-16 19:29:13 +0100

    We can now retrieve batches

---
+++ batches.go
@@ -0,0 +1,81 @@
+package brewchild
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+)
+
+type Hop struct {
+	Origin string  `json:"origin"`
+	ID     string  `json:"_id"`
+	Name   string  `json:"name"`
+	Amount float64 `json:"amount"`
+	Alpha  float64 `json:"alpha"`
+	Type   string  `json:"type"`
+}
+
+type Batch struct {
+	ID                 string    `json:"_id"`
+	Name               string    `json:"name"`
+	BatchNumber        int       `json:"batchNo"`
+	Status             string    `json:"status"`
+	Brewer             string    `json:"brewer"`
+	BrewDate           *DateTime `json:"brewDate"`
+	CarbonationType    string    `json:"carboationType"`
+	BottlingDate       *DateTime `json:"bottlingDate"`
+	Notes              []string  `json:"notes"`
+	EstimatedIBU       int       `json:"estimatedIbu"`
+	MeasuredABV        float64   `json:"measuredAbv"`
+	EstimatedBuGuRatio float64   `json:"estimatedBuGuRatio"`
+	EstimatedOG        float64   `json:"estimatedOg"`
+	EstimatedColor     float64   `json:"estimatedColor"`
+	Hops               []*Hop    `json:"batchHops"`
+	IBU                int       `json:"ibu"`
+	OG                 float64   `json:"og"`
+	OGPlato            float64   `json:"ogPlato"`
+	ABV                float64   `json:"abv"`
+	FG                 float64   `json:"fg"`
+	Nutrition          struct {
+		Calories struct {
+			Total float64 `json:"total"`
+		} `json:"calories"`
+		Carbs struct {
+			Total float64 `json:"total"`
+		} `json:"carbs"`
+	} `json:"nutrition"`
+	BuGuRatio float64 `json:"buGuRatio"`
+	Author    string  `json:"author"`
+}
+
+func (c *Client) Batches(opts ...listOpt) ([]*Batch, error) {
+	url, err := url.Parse(c.apiBase + "batches")
+	if err != nil {
+		return nil, fmt.Errorf("Failed to parse brewfather URL: %w", err)
+	}
+
+	for _, o := range opts {
+		url = o(url)
+	}
+	req, err := http.NewRequest(http.MethodGet, url.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to create request to brewfather: %w", err)
+	}
+
+	resp, err := c.h.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to query the brewfather server: %w", err)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("Failed to read response from brewfather: %w", err)
+	}
+	batches := []*Batch{}
+	if err := json.Unmarshal(body, &batches); err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal response from brewfather: %w", err)
+	}
+	return batches, nil
+}
---
+++ brewchild.go
@@ -0,0 +1,107 @@
+package brewchild
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"strconv"
+	"time"
+)
+
+type DateTime struct {
+	t time.Time
+}
+
+func (d *DateTime) UnmarshalJSON(in []byte) error {
+	ts, err := strconv.ParseInt(string(in), 10, 64)
+	if err != nil {
+		return err
+	}
+	d.t = time.Unix(ts/1000, 0)
+	return nil
+}
+
+func (d *DateTime) String() string {
+	return d.t.Format(time.RFC3339)
+}
+
+type Client struct {
+	h *http.Client
+
+	apiBase string
+}
+
+type listOpt func(*url.URL) *url.URL
+
+func Complete(compl bool) listOpt {
+	return func(in *url.URL) *url.URL {
+		q := in.Query()
+		q.Add("complete", fmt.Sprintf("%t", compl))
+		in.RawQuery = q.Encode()
+		return in
+	}
+}
+
+func Status(status string) listOpt {
+	return func(in *url.URL) *url.URL {
+		q := in.Query()
+		q.Add("status", status)
+		in.RawQuery = q.Encode()
+		return in
+	}
+}
+
+func Offset(off int) listOpt {
+	return func(in *url.URL) *url.URL {
+		q := in.Query()
+		q.Add("offset", fmt.Sprintf("%d", off))
+		in.RawQuery = q.Encode()
+		return in
+	}
+}
+
+func Limit(limit int) listOpt {
+	return func(in *url.URL) *url.URL {
+		q := in.Query()
+		q.Add("limit", fmt.Sprintf("%d", limit))
+		in.RawQuery = q.Encode()
+		return in
+	}
+}
+
+type brewfatherTransport struct {
+	http.Transport
+
+	auth string
+}
+
+func (b *brewfatherTransport) RoundTrip(r *http.Request) (*http.Response, error) {
+	r.Header.Add("Authorization", "Basic "+b.auth)
+	resp, err := b.Transport.RoundTrip(r)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode > 299 {
+		body, err := ioutil.ReadAll(resp.Body)
+		defer resp.Body.Close()
+		if err != nil {
+			return nil, fmt.Errorf("Failed to read error response: %w", err)
+		}
+		return nil, fmt.Errorf("Received error from brewfather: %s", string(body))
+	}
+	return resp, err
+}
+
+func New(userID, apiKey string) (*Client, error) {
+	auth := base64.RawURLEncoding.EncodeToString([]byte(userID + ":" + apiKey))
+	c := &Client{
+		h: &http.Client{
+			Transport: &brewfatherTransport{auth: auth},
+		},
+		apiBase: "https://api.brewfather.app/v1/",
+	}
+
+	return c, nil
+}
